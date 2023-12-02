28b206b64b19f1e9e00a9c7037cf3f53df941ef5
hsh108868 <pusrty@gmail.com>
2023-12-02 23:39:16 +0900

    update terraform files

--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -1,3 +1,5 @@
-project_id   = "k8s-standard-architecture"
+project_id   = "spry-framework-406704"
 cluster_name = "k8s-standard-architecture"
 region       = "asia-northeast3"
+
+# variables.tf 파일에 정의된 변수에 값을 주입한다. 보통 Variables = Value 형태로 정의한다.
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,37 @@
+# Common variables
+
 variable "project_id" {
   type        = string
-  description = "플젝 이름"
+  description = "project id"
   default     = ""
 }
+
+variable "region" {
+  type        = string
+  description = "cluster region"
+  default     = ""
+}
+
+variable "env_name" {
+  type        = string
+  description = "The environment for the GKE cluster"
+  default     = "dev"
+}
+
+# Cluster variables
+
+
+variable "cluster_name" {
+  type        = string
+  description = "name of cluster"
+  default     = ""
+}
+
+variable "network" {
+  type        = string
+  description = "The VPC network"
+  default     = "gke-network"
+}
+
+
+# 변수를 정의한다. 정의한 변수에 값을 주입하기 위해서는 terraform.tfvars 파일을 이용한다.
